7766e5c2c61f8d409aa7298e1a8fc03dd9326dc7
Gobinda Roy <61370554+Gobinda-Roy7@users.noreply.github.com>
2020-05-06 05:26:13 +0600

    Add files via upload

--- resource/js/script.js
+++ resource/js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('.slider').slider();
+    
+});
+$(function(){
+    var mixer = mixitup('.price');
+    //for menu active class
+    $('.portfolio-filter button').on('click', function(event) {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
+   
 });
